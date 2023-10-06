c5b3eddab12518c967d8ac2bc32216e51089adf1
Niranjan Salunkhe <101391587+nsalunkhe@users.noreply.github.com>
2023-10-06 16:35:26 +0530

    added

---
+++ Frontend/src/App.js
@@ -0,0 +1,110 @@
+import React, { useState, useEffect } from 'react';
+
+const App = () => {
+  const [value, setValue] = useState(null);
+  const [message, setMessage] = useState(null);
+  const [previousChats, setPreviousChats] = useState([]);
+  const [currentTitle, setCurrentTitle] = useState(null);
+
+  const createNewChat = () => {
+    setMessage(null);
+    setValue('');
+    setCurrentTitle(null);
+  };
+
+  const handleClick = (uniqueTitle) => {
+    setCurrentTitle(uniqueTitle);
+    setMessage(null);
+    setValue('');
+  };
+
+  const getMessages = async () => {
+    const options = {
+      method: 'POST',
+      body: JSON.stringify({
+        message: value
+      }),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    };
+
+    try {
+      const response = await fetch('https://gptbackend-tm8d.onrender.com/completions', options);
+      const data = await response.json();
+      setMessage(data.choices[0].message);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    console.log(currentTitle, value, message);
+    if (!currentTitle && value && message) {
+      setCurrentTitle(value);
+    }
+    if (currentTitle && value && message) {
+      setPreviousChats((prevChats) => [
+        ...prevChats,
+        {
+          title: currentTitle,
+          role: 'user',
+          content: value
+        },
+        {
+          title: currentTitle,
+          role: message.role,
+          content: message.content
+        }
+      ]);
+    }
+  }, [message, currentTitle]);
+
+  const currentChat = previousChats.filter((previousChat) => previousChat.title === currentTitle);
+  const uniqueTitles = Array.from(new Set(previousChats.map((previousChat) => previousChat.title)));
+
+  return (
+    <div className="app">
+      <section className="side-bar">
+        <button onClick={createNewChat}>New Chat</button>
+        <ul className="history">
+          {uniqueTitles?.map((uniqueTitle, index) => (
+            <li key={index} onClick={() => handleClick(uniqueTitle)}>
+              {uniqueTitle}
+            </li>
+          ))}
+        </ul>
+        <nav>
+          <p style={{ color: 'white', fontStyle: 'cursive' }}>Made by Niranjan</p>
+        </nav>
+      </section>
+      <section className="main">
+        {!currentTitle && <h1 style={{ color: 'olive' }}>MyGPT</h1>}
+        <ul className="feed">
+          {currentChat?.map((chatMessage, index) => (
+            <li key={index}>
+              <p className="role">{chatMessage.role}</p>
+              {/* Display the response content in a pre element */}
+              <pre style={{ whiteSpace: 'pre-wrap', backgroundColor: '#f4f4f4', padding: '10px', borderRadius: '5px' }}>
+                {chatMessage.content}
+              </pre>
+            </li>
+          ))}
+        </ul>
+        <div className="bottom-section">
+          <div className="input-container">
+            <input value={value} onChange={(e) => setValue(e.target.value)} placeholder="Send a message" />
+            <div id="submit" onClick={getMessages}>
+              GO
+            </div>
+          </div>
+          <h3 className="info" style={{ color: 'white' }}>
+            Welcome!
+          </h3>
+        </div>
+      </section>
+    </div>
+  );
+};
+
+export default App;
